d281750e325cff9e819358b5554a251ed5b225d9
William <rvw112@gmail.com>
2023-02-14 22:42:08 +1300

    Implementing tiles file and testing tile draw

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame, sys
 from settings import *
+from tiles import Tile
 
 # pygame setup
 pygame.init()
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
+test_tile = pygame.sprite.Group(Tile((100,100), 200))
 
 while True:
     for event in pygame.event.get():
@@ -14,7 +16,7 @@ while True:
             sys.exit()
 
     screen.fill('black')
-
+    test_tile.draw(screen)
     pygame.display.update()
     clock.tick(60)
 
