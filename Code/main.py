83acd561325d70db0777a38d16e44d7c9e27f48c
William <rvw112@gmail.com>
2023-02-15 16:18:03 +1300

    Implementing base files and sprites

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 from tiles import Tile
 
 # pygame setup
@@ -7,7 +8,10 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('2D python game')
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
@@ -17,6 +21,7 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
--- Code/tiles.py
+++ Code/tiles.py
@@ -1,8 +1,8 @@
 import pygame
+from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,pos,size):
-        super().__init__()
-        self.image = pygame.Surface((size, size))
-        self.image.fill('grey')
+    def __init__(self,pos,groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('../Graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
