46baade36d35cfff727dc93f350a61b4753a8f73
William <rvw112@gmail.com>
2023-02-20 16:49:11 +1300

    Implementing player movement controls

--- Code/player.py
+++ Code/player.py
@@ -6,3 +6,36 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('../Graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        # Up, down and neutral directional keys
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        # Left, right and neutral directional keys
+        if keys[pygame.K_d]:
+            self.direction.x = 1
+        elif keys[pygame.K_a]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    # Move the player sprite
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        self.rect.center += self.direction * speed
+
+    # Update
+    def update(self):
+        self.input()
+        self.move(self.speed)
